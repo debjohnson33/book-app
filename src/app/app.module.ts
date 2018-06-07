35b0be8d5753e95eaa876024e699461da37ea62c
debjohnson <debjohnson_11@yahoo.com>
2018-06-06 22:08:14 -0500

    Added bootstrap & dependencies/started to add classes to html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
@@ -11,7 +12,8 @@ import { AppService } from './app.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule.forRoot()
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
